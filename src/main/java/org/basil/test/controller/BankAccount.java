8e86854266cf0f65dedbf60f04aad74fb5f6d092
Basil135 <basil135@mail.ru>
2018-05-21 01:10:24 +0300

    add to a h2 and hibernate

--- src/main/java/org/basil/test/controller/BankAccount.java
+++ src/main/java/org/basil/test/controller/BankAccount.java
@@ -4,17 +4,39 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.sun.org.glassfish.external.probe.provider.annotations.ProbeParam;
 import org.basil.test.model.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Path("bankaccount")
 public class BankAccount {
 
-    private static final Map<String, Account> accounts = new ConcurrentHashMap<>();
+    private static String regex = "^[0-9]{5}$";
+
+    private static SessionFactory sessionFactory;
+    private static Session session;
+
+    private Session getSession() {
+        if (session == null) {
+            sessionFactory = getSessionFactory();
+            session = sessionFactory.openSession();
+        }
+        return session;
+    }
+
+    private SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration()
+                    .configure()
+                    .addAnnotatedClass(Account.class)
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -23,10 +45,14 @@ public class BankAccount {
     public Response getBill(
             final @PathParam("id") String id
     ) {
-        if (id == null || !id.matches("^[0-9]{5}$")) {
+        if (id == null || !id.matches(regex)) {
             return Response.notModified("The account number should consist of five digits!").build();
         }
-        Account account = accounts.get(id);
+
+        Session session = getSession();
+
+        Account account = session.get(Account.class, id);
+
         if (account == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
@@ -39,13 +65,20 @@ public class BankAccount {
     public Response addBill(
             final @PathParam("id") String id
     ) {
-        if (!id.matches("^[0-9]{5}$")) {
+        if (!id.matches(regex)) {
             return Response.notModified("The bill number should consist of five digits!").build();
         }
-        if (accounts.containsKey(id)) {
+
+        Session session = getSession();
+
+        Account account = session.get(Account.class, id);
+
+        if (account != null) {
             return Response.notModified("The bill is already exist!").build();
         }
-        accounts.put(id, new Account(id));
+
+        session.save(new Account(id));
+
         return Response.ok().build();
     }
 
@@ -55,32 +88,44 @@ public class BankAccount {
     @Path("{id}/withdraw")
     public Response withdraw(
             final @PathParam("id") String id,
-            final @ProbeParam("withdraw") String strCountMoney
+            final @ProbeParam("money") String strCountMoney
     ) {
-        if (!id.matches("^[0-9]{5}$")) {
+        if (!id.matches(regex)) {
             return Response.notModified("The account number should consist of five digits!").build();
         }
+
         JsonObject json = new Gson().fromJson(strCountMoney, JsonObject.class);
-        String money = json.get("withdraw").getAsString();
+        String money = json.get("money").getAsString();
+
         Integer countMoney;
         try {
             countMoney = Integer.parseInt(money);
         } catch (ClassCastException e) {
             return Response.notModified("The count of money should be double!").build();
         }
+
         if (countMoney < 0) {
             return Response.notModified("The count of money should be greater then zero!").build();
         }
-        Account account = accounts.get(id);
+
+        Session session = getSession();
+
+        Account account = session.get(Account.class, id);
+
         if (account == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+
         Integer balance = account.getDeposit() - countMoney;
+
         if (balance < 0) {
             return Response.notModified("Not enough money at the account!").build();
         }
+
         account.setDeposit(balance);
-        accounts.replace(id, account);
+
+        session.saveOrUpdate(account);
+
         return Response.ok().build();
     }
 
@@ -90,31 +135,39 @@ public class BankAccount {
     @Path("{id}/deposit")
     public Response deposit(
             final @PathParam("id") String id,
-            final @ProbeParam("deposit") String strCountMoney
+            final @ProbeParam("money") String strCountMoney
     ) {
-        if (!id.matches("^[0-9]{5}$")) {
+        if (!id.matches(regex)) {
             return Response.notModified("The account number should consist of five digits!").build();
         }
+
         JsonObject json = new Gson().fromJson(strCountMoney, JsonObject.class);
-        String money = json
-                .get("deposit")
-                .getAsString();
+        String money = json.get("money").getAsString();
+
         Integer countMoney;
         try {
             countMoney = Integer.parseInt(money);
         } catch (Exception e) {
             return Response.notModified("The count of money should be double!").build();
         }
+
         if (countMoney < 0) {
             return Response.notModified("The count of money should be greater then zero!").build();
         }
-        Account account = accounts.get(id);
+
+        Session session = getSession();
+
+        Account account = session.get(Account.class, id);
+
         if (account == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+
         Integer balance = account.getDeposit() + countMoney;
         account.setDeposit(balance);
-        accounts.replace(id, account);
+
+        session.saveOrUpdate(account);
+
         return Response.ok().build();
     }
 }
--- src/main/java/org/basil/test/model/Account.java
+++ src/main/java/org/basil/test/model/Account.java
@@ -2,9 +2,18 @@ package org.basil.test.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table
 public class Account {
 
+    @Id
     private final String id;
+    @Column
     private Integer  deposit = 0;
 }
